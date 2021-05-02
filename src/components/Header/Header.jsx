a6bf242dc8c358fe7c84a51163e533f14fb83588
validName <>
2021-05-02 08:29:46 -0500

    details

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,7 +24,6 @@ class Header extends Component {
       return (
          <div>
             <img src={iconHeader} alt="" className='iconHeader'/>
-            <h3 className='fundationNameHeader'>Fundaciòn Lolita Barragàn</h3>
             <div className='containerHeader'>
             <span href='/' onClick={this.openModal} className='modalNavegation'><i className='fa fa-bars'></i></span>
                <div className='infoHeader'>
