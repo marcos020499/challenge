71eec26eedb3599b5eb47e805b6c05064a2db9e0
validName <>
2021-05-04 07:53:13 -0500

    lazy load

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, { lazy, Suspense } from 'react';
 import Header from './components/Header/Header'
 import ContentFirstSection from './components/Content/firstSection/ContentFirstSection'
 import ContentSecontSection from './components/Content/secondSection/ContentSecontSection'
 import Footer from './components/Footer/Footer'
-
+const ContentSecondSection = lazy(()=>import('./components/Content/secondSection/ContentSecontSection'))
 const App = () => {
   return (
     <div>
       <Header/>
-      <ContentFirstSection/>
-      <ContentSecontSection/>
+      <Suspense fallback={<h1>Still Loading…</h1>}>
+        <ContentSecondSection/>
+        <ContentSecondSection/>
+        </Suspense>
       <Footer/>
     </div>
   );
